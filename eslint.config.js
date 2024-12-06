e0493414b3d1dd16e04e54de06abb8dc6598baf6
RipperZero <1018744707@qq.com>
2024-12-06 16:47:48 +0900

    feat: update eslint to 9

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import configPrettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+import js from "@eslint/js";
+
+export default tseslint.config(
+  configPrettier,
+  { ignores: ["dist", "node_modules", "vite.config.mts"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    // files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      // "react/jsx-pascal-case": ["warn", { allowLeadingUnderscore: true }],
+    },
+  },
+);
